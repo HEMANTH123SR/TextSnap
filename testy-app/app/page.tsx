0634efb5862cbfdd86f9f7c18e51850c3fdba24c
HEMANTH123SR <applicationcomputer257@gmail.com>
2023-12-17 13:53:55 +0530

    intial commit

---
+++ testy-app/app/page.tsx
@@ -0,0 +1,11 @@
+import { LandignPage } from "@/components/component/landign-page";
+
+const Page = () => {
+  return (
+    <div style={{ backgroundColor: "#FFFFFF" }}>
+      <LandignPage />
+    </div>
+  );
+};
+
+export default Page;
